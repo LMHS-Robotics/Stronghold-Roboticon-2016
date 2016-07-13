83073203d995007e2e740efbc51e709507682044
Nerds_of_Prey <nerdsofprey@hotmail.com>
2016-07-12 23:43:34 -0400

    Update POVButton.cpp

--- src/POVButton.cpp
+++ src/POVButton.cpp
@@ -7,6 +7,8 @@
 
 #include "POVButton.h"
 
+//this class is used to convert POV angles into buttons to be used for command based programming. 
+
 POVButton::POVButton(GenericHID* joystick, int povangle) {
 
 	m_joystick = joystick;
