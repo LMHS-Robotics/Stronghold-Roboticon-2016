448d41f636ab4f8f989681f5477f3fa47cebd794
Nerds_of_Prey <nerdsofprey@hotmail.com>
2016-07-14 23:59:10 -0400

    Update JoystickAnalogButton.cpp

--- src/JoystickAnalogButton.cpp
+++ src/JoystickAnalogButton.cpp
@@ -12,16 +12,16 @@
 
 JoystickAnalogButton::JoystickAnalogButton(GenericHID* joystick, int axisNumber){
 	// TODO Auto-generated constructor stub
-		  THRESHOLD = 0.1;
+		  THRESHOLD = 0.1;//how far axis must go for it to be triggered as a button
 
-	      m_joystick = joystick;
-	      m_axisNumber = axisNumber;
+	      m_joystick = joystick;//joystick variable
+	      m_axisNumber = axisNumber;//joystick axis number
 
 }
 
 
-bool JoystickAnalogButton::Get() {
-    if(m_joystick->GetRawAxis(m_axisNumber) > THRESHOLD){
+bool JoystickAnalogButton::Get() {///only function that is virutal is the Get() from trigger, which is called here
+    if(m_joystick->GetRawAxis(m_axisNumber) > THRESHOLD){//checks for the axis moving
       return m_joystick->GetRawAxis(m_axisNumber) > THRESHOLD;    //Return true if axis value is less than negative threshold
     } else {
       return m_joystick->GetRawAxis(m_axisNumber) > THRESHOLD;    //Return false if axis value is greater than positive threshold
